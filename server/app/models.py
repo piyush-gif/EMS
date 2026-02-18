f134791252bfe0d92dccb99a3598545d404d4e03
piyush <piyush.khadka2233@gmail.com>
2026-02-18 15:20:40 +0000

    database models

--- server/app/models.py
+++ server/app/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime
 from sqlalchemy.sql import func
-from app.database import Base
+from database import Base
 
 class User(Base):
     __tablename__ = "users"
